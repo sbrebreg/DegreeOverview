db0b41778e06f094f17e764f0d1044d8c2b25a9e
sbnikiec <niklas.kiechle@student.hs-neu-ulm.de>
2016-01-04 15:13:15 +0100

    Current State

--- DegreeOverview/WebContent/js/login.js
+++ DegreeOverview/WebContent/js/login.js
@@ -1,6 +1,6 @@
 //Login
 
-$(function() {
+$(function () {
 
     var $formLogin = $('#login-form');
     var $formLost = $('#lost-form');
@@ -11,10 +11,10 @@ $(function() {
     var $msgShowTime = 2000;
 
     $("form").submit(function () {
-        switch(this.id) {
+        switch (this.id) {
             case "login-form":
-                var $lg_username=$('#login_username').val();
-                var $lg_password=$('#login_password').val();
+                var $lg_username = $('#login_username').val();
+                var $lg_password = $('#login_password').val();
                 if ($lg_username == "ERROR") {
                     msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Login error");
                 } else {
@@ -23,7 +23,7 @@ $(function() {
                 return false;
                 break;
             case "lost-form":
-                var $ls_email=$('#lost_email').val();
+                var $ls_email = $('#lost_email').val();
                 if ($ls_email == "ERROR") {
                     msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "error", "glyphicon-remove", "Send error");
                 } else {
@@ -32,9 +32,9 @@ $(function() {
                 return false;
                 break;
             case "register-form":
-                var $rg_username=$('#register_username').val();
-                var $rg_email=$('#register_email').val();
-                var $rg_password=$('#register_password').val();
+                var $rg_username = $('#register_username').val();
+                var $rg_email = $('#register_email').val();
+                var $rg_password = $('#register_password').val();
                 if ($rg_username == "ERROR") {
                     msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Register error");
                 } else {
@@ -48,26 +48,38 @@ $(function() {
         return false;
     });
 
-    $('#login_register_btn').click( function () { modalAnimate($formLogin, $formRegister) });
-    $('#register_login_btn').click( function () { modalAnimate($formRegister, $formLogin); });
-    $('#login_lost_btn').click( function () { modalAnimate($formLogin, $formLost); });
-    $('#lost_login_btn').click( function () { modalAnimate($formLost, $formLogin); });
-    $('#lost_register_btn').click( function () { modalAnimate($formLost, $formRegister); });
-    $('#register_lost_btn').click( function () { modalAnimate($formRegister, $formLost); });
+    $('#login_register_btn').click(function () {
+        modalAnimate($formLogin, $formRegister)
+    });
+    $('#register_login_btn').click(function () {
+        modalAnimate($formRegister, $formLogin);
+    });
+    $('#login_lost_btn').click(function () {
+        modalAnimate($formLogin, $formLost);
+    });
+    $('#lost_login_btn').click(function () {
+        modalAnimate($formLost, $formLogin);
+    });
+    $('#lost_register_btn').click(function () {
+        modalAnimate($formLost, $formRegister);
+    });
+    $('#register_lost_btn').click(function () {
+        modalAnimate($formRegister, $formLost);
+    });
 
-    function modalAnimate ($oldForm, $newForm) {
+    function modalAnimate($oldForm, $newForm) {
         var $oldH = $oldForm.height();
         var $newH = $newForm.height();
-        $divForms.css("height",$oldH);
-        $oldForm.fadeToggle($modalAnimateTime, function(){
-            $divForms.animate({height: $newH}, $modalAnimateTime, function(){
+        $divForms.css("height", $oldH);
+        $oldForm.fadeToggle($modalAnimateTime, function () {
+            $divForms.animate({height: $newH}, $modalAnimateTime, function () {
                 $newForm.fadeToggle($modalAnimateTime);
             });
         });
     }
 
-    function msgFade ($msgId, $msgText) {
-        $msgId.fadeOut($msgAnimateTime, function() {
+    function msgFade($msgId, $msgText) {
+        $msgId.fadeOut($msgAnimateTime, function () {
             $(this).text($msgText).fadeIn($msgAnimateTime);
         });
     }
@@ -78,7 +90,7 @@ $(function() {
         $divTag.addClass($divClass);
         $iconTag.removeClass("glyphicon-chevron-right");
         $iconTag.addClass($iconClass + " " + $divClass);
-        setTimeout(function() {
+        setTimeout(function () {
             msgFade($textTag, $msgOld);
             $divTag.removeClass($divClass);
             $iconTag.addClass("glyphicon-chevron-right");
